8fe7b14c95674ce89d7aa24a542c4502c8a96135
cly <854450468@qq.com>
2023-11-14 11:13:36 +0800

    1

---
+++ spring6-ioc-003/src/main/java/com/lingyu/bean/UserFactoryBean.java
@@ -0,0 +1,15 @@
+package com.lingyu.bean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class UserFactoryBean implements FactoryBean<User> {
+    @Override
+    public User getObject() throws Exception {
+        return new User();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return User.class;
+    }
+}
--- spring6-ioc-003/src/test/java/TestLifeCycle.java
+++ spring6-ioc-003/src/test/java/TestLifeCycle.java
@@ -1,5 +1,6 @@
 import com.lingyu.bean.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestLifeCycle {
@@ -19,5 +20,14 @@ public class TestLifeCycle {
         User bean = ac.getBean(User.class);
         System.out.println("生命周期：4、通过IOC容器获取bean并使用");
         ac.close();
+    }
+
+
+    @Test
+    public void testUserFactoryBean(){
+        //获取IOC容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+        User user = (User) ac.getBean("user");
+        System.out.println(user);
     }
 }
